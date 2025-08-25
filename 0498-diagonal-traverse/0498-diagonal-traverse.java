77e0a67ac1497e1f67d41dcea48d30d7efb33acd
NITHESH M P <nitheshmp425@gmail.com>
2025-08-25 15:09:43 +0530

    Time: 2 ms (99.41%), Space: 47.1 MB (70.4%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -4,56 +4,52 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
         int[] result = new int[m * n];
-        int r = 0;
-        int c = 0;
-        int  dir = 0;
-        int k = 0;
+        int k = 0, dir = 0;
+        int r = 0, c = 0;
 
-        for(int i = 0; i < m + n - 1; i++)
+        while(k < m * n)
         {
+            result[k++] = mat[r][c]; 
+
             if(dir == 0)
             {
-                if(i < m)
+                if(c == n-1)
                 {
-                    r = i;
-                    c = 0;
+                    r++;
+                    dir = 1;
+                }
+                else if(r == 0)
+                {
+                    c++;
+                    dir = 1;
                 }
                 else
                 {
-                    r = m - 1;
-                    c = i - (m - 1);
-                }
-                while(r >= 0 && c < n)
-                {
-                    result[k++] = mat[r][c];
                     r--;
                     c++;
                 }
-                dir = 1;
             }
             else
             {
-                if(i < n)
+                if(r == m - 1)
                 {
-                    r = 0;
-                    c = i;
+                    c++;
+                    dir = 0;
+                }
+                else if(c == 0)
+                {
+                    r++;
+                    dir = 0;
                 }
                 else
                 {
-                    r = i - (n - 1);
-                    c = n - 1;
-                }
-                while(r < m && c >= 0)
-                {
-                    result[k++] = mat[r][c];
                     r++;
                     c--;
                 }
-                dir = 0;
             }
 
-        }   
+        }
 
-        return result; 
+        return result;        
     }
 }
