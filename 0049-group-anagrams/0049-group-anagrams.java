b59fb3643511c20bda6939a48964d9e96a901804
NITHESH M P <nitheshmp425@gmail.com>
2026-01-02 23:36:28 +0530

    Time: 7 ms (75.32%), Space: 49.2 MB (97.13%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,42 +1,40 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) 
-    {
+    public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> result = new ArrayList<>();
 
-        if(strs.length == 0 || strs.length == 1)
-        {
-            result.add(new ArrayList<>(Arrays.asList(strs[0])));
+        if(strs.length <= 1){
+            result.add(Arrays.asList(strs[0]));
             return result;
         }
 
-        String[] temp = new String[strs.length];
-        int i = 0;
+        HashMap<String, List<Integer>> map = new HashMap<>();
 
-        for(String s : strs)
-        {
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
+        for(int i = 0; i < strs.length; i++){
+            String s = sort(strs[i]);
 
-            temp[i++] = new String(arr); 
+            map.putIfAbsent(s, new ArrayList<>());
+            map.get(s).add(i);
         }
 
-        HashMap<String, List<String>> map = new HashMap<>();
+        for(Map.Entry<String, List<Integer>> entry : map.entrySet()){
+            List<Integer> list = entry.getValue();
+            List<String> anagram = new ArrayList<>();
 
-        for(i = 0; i < strs.length; i++)
-        {
-            String key = temp[i];
-
-            if(map.containsKey(key))
-                map.get(key).add(strs[i]);
-            else
-            {
-                map.put(key, new ArrayList<>());
-                map.get(key).add(strs[i]);
+            for(int i = 0; i < list.size(); i++){
+                anagram.add(strs[list.get(i)]);
             }
 
+            result.add(anagram);
         }
 
-        result.addAll(map.values());
+
         return result;
+        
+    }
+
+    public String sort(String s){
+        char[] a = s.toCharArray();
+        Arrays.sort(a);
+        return new String(a);
     }
 }
